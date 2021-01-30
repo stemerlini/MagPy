925f47eb488d4cdf848b294310465421be13a39d
Jack Halliday <jack.halliday12@imperial.ac.uk>
2021-01-30 14:12:47 +0000

    Added profile method to Image.

--- MagPy/Imaging.py
+++ MagPy/Imaging.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage.transform as sk_t
+from skimage.measure import profile_line
 
 
 class Image:
@@ -13,10 +14,11 @@ class Image:
         self.im = sk_t.rotate(image, rotate, resize=True)
         self.sc = pxpermm
         self.o = np.array([0., 0.])
+        self.shape = image.shape
         
     def px_to_mm(self, p_px):
         '''Calculates position of point in mm, given position in px'''
-        h = self.im.shape[0]
+        h = self.shape[0]
         p = np.array(p_px, dtype=np.float64)
         p *= np.array([1., -1.]) #Convert handedness 
         p += np.array([0., h]) #Translate origin to BL corner
@@ -25,7 +27,7 @@ class Image:
         return p
     def mm_to_px(self, p_mm):
         '''Calculates position of point in px, given position in mm'''
-        h = self.im.shape[0]
+        h = self.shape[0]
         p = np.array(p_mm)
         p += self.o
         p *= self.sc
@@ -55,9 +57,17 @@ class Image:
         x0, y0 = self.px_to_mm([x0, y0])
         x1, y1 = self.px_to_mm([x1, y1])
         extent = [x0, x1, y1, y0]
-        ax.imshow(self.im, extent=extent, **kwargs)
+        return ax.imshow(self.im, extent=extent, **kwargs)
         
     def plot_px(self, ax, **kwargs):
         '''Plot image with axes pixels. kwargs are passed
         to plt.imshow method.'''
-        ax.imshow(self.im, **kwargs)
+        return ax.imshow(self.im, **kwargs)
+        
+    def profile_mm(self, src_mm, dst_mm, width_mm, **kwargs):
+        src_px = np.flip( self.mm_to_px(src_mm) )
+        dst_px = np.flip( self.mm_to_px(dst_mm) )
+        width_px = int(width_mm*self.sc)
+        p = profile_line(self.im, src_px, dst_px, linewidth=width_px, **kwargs)
+        r = np.linspace(src_mm, dst_mm, len(p))
+        return r, p
