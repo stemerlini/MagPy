300b98c1329e7e4f459f3d079ba4d20996dd8d7a
stemerlini <s.merlini19@imperial.ac.uk>
2022-04-14 09:44:31 +0100

    updated version

---
+++ Imaging.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.transform as sk_t
+from skimage.measure import profile_line
+
+
+class Image:
+    '''Initialize Image class. Parameters:
+    1) image - greyscale image represented as numpy array
+    2) rotation - rotation to be applied to image in degrees
+    3) pxpermm - image scale in pixels per mm'''
+            
+    def __init__(self, image, rotate, pxpermm):
+        self.im = sk_t.rotate(image, rotate, resize=False)
+        self.sc = pxpermm
+        self.o = np.array([0., 0.])
+        self.shape = image.shape
+        self.r = rotate
+        
+    def px_to_mm(self, p_px):
+        '''Calculates position of point in mm, given position in px'''
+        h = self.shape[0]
+        p = np.array(p_px, dtype=np.float64)
+        p *= np.array([1., -1.]) #Convert handedness 
+        p += np.array([0., h]) #Translate origin to BL corner
+        p /= self.sc
+        p -= self.o
+        return p
+    def mm_to_px(self, p_mm):
+        '''Calculates position of point in px, given position in mm'''
+        h = self.shape[0]
+        p = np.array(p_mm)
+        p += self.o
+        p *= self.sc
+        p *= np.array([1., -1.]) #Convert handedness 
+        p += np.array([0., h]) #Translate origin to TR corner
+        return np.array(p, dtype=np.int64)
+    
+    def set_origin(self, p_px):
+        '''Sets origin of image from value in pixels'''
+        self.o  = np.array([0., 0.])
+        p_mm = self.px_to_mm(p_px)
+        self.o = p_mm
+        self.o_px = p_px
+        
+    def get_origin(self):
+        '''Returns the position of the origin in pixels'''
+        o = np.array([0., 0.])
+        o_px = self.mm_to_px(o)
+        return o_px
+    
+    def plot_mm(self, ax, multiply_by = None, mask = None, **kwargs):
+        '''Plot image with axes in physical units. kwargs are passed
+        to plt.imshow method.'''
+        x0 = 0
+        x1 = self.im.shape[1]
+        y0 = 0
+        y1 = self.im.shape[0]
+        x0, y0 = self.px_to_mm([x0, y0])
+        x1, y1 = self.px_to_mm([x1, y1])
+        extent = [x0, x1, y1, y0]
+
+        if multiply_by:
+            if mask:
+                self.masked_im = np.ma.masked_less_equal(self.im, mask)
+                return ax.imshow(self.masked_im*multiply_by, extent=extent, **kwargs)
+            else:
+                return ax.imshow(self.im*multiply_by, extent=extent, **kwargs)
+        else:
+            return ax.imshow(self.im, extent=extent, **kwargs)
+
+        
+    def plot_px(self, ax, **kwargs):
+        '''Plot image with axes pixels. kwargs are passed
+        to plt.imshow method.'''
+        return ax.imshow(self.im, **kwargs)
+        
+    def profile_mm(self, src_mm, dst_mm, width_mm, **kwargs):
+        src_px = np.flip( self.mm_to_px(src_mm) )
+        dst_px = np.flip( self.mm_to_px(dst_mm) )
+        width_px = int(width_mm*self.sc)
+        p = profile_line(self.im, src_px, dst_px, linewidth=width_px, **kwargs)
+        r = np.linspace(src_mm, dst_mm, len(p))
+        return r, p
+        
+    def create_im(self, im):
+        out = Image(im, 0., self.sc)
+        out.set_origin(self.o_px)
+        return out
+        
---
+++ Scopes.py
@@ -0,0 +1,96 @@
+import numpy as np
+from scipy.integrate import cumtrapz
+from scipy.fft import rfft, rfftfreq, irfft
+class Scopes:
+    def __init__(self, shot, currentStart=1400., path=r'//LINNA/scopes/'):
+        self.sh = shot
+        self.pth = path
+        self.start = float(currentStart)
+    def getCh(self, scopeNumber, channel):
+        voltName = 'scope' + str(scopeNumber)
+        if(channel[0]=='E'):
+            voltName += 'E'
+        voltName += '_' + self.sh
+        timeName = voltName + 'time'
+        voltName += '_' + channel
+        volts = np.genfromtxt(self.pth+voltName)[0:-1]
+        time = np.genfromtxt(self.pth+timeName)
+        return time, volts
+    def getRog1(self):
+        return self.getCh(2,'C1')
+    def getRog2(self):
+        return self.getCh(2,'C2')
+    def getRogA(self):
+        return self.getCh(1,'D1')
+    def getRogB(self):
+        return self.getCh(1, 'D2')
+    def getdIdt_G(self):
+        return self.getCh(10,'A1')
+    def getdIdt_H(self):
+        return self.getCh(10,'A2')
+    def getdIdt_C(self):
+        return self.getCh(10,'B1')
+    def getdIdt_Z(self):
+        return self.getCh(10,'B2')
+    def getLine_G(self):
+        return self.getCh(10,'C1')
+    def getLine_H(self):
+        return self.getCh(10,'C2')
+    def getLine_C(self):
+        return self.getCh(10,'D1')
+    def getLine_Z(self):
+        return self.getCh(10,'D2')
+    def getMarx_G(self):
+        return self.getCh(11,'C1')
+    def getMarx_H(self):
+        return self.getCh(11,'C2')
+    def getMarx_C(self):
+        return self.getCh(11,'D1')
+    def getMarx_Z(self):
+        return self.getCh(11,'D2')
+        
+class Channel:
+    def __init__(self, scope_object, scope_number, channel_key, attenuation):
+        self.currentStart = scope_object.start
+        self.t, self.v = scope_object.getCh(scope_number, channel_key)
+        self.v *= attenuation
+    def low_pass(self, cuttoff_s):
+        c_ns = cuttoff_s*1e-9
+        v_fft = rfft(self.v)
+        N = self.t.shape[0]
+        dt = self.t[1]-self.t[0]
+        f = rfftfreq(N, dt)
+        window = f<c_ns
+        self.v_filt = irfft(v_fft*window)
+        
+class Rogowski:
+    def __init__(self, channel, calibration=3.):
+        self.channel = channel
+        self.calibration = calibration
+    def getInductiveComponent(self, other_probe):
+        t = self.channel.t - self.channel.currentStart
+        v1 = self.channel.v_filt
+        v2 = other_probe.channel.v_filt       
+        inductive = (v1 - v2)*0.5
+        return t, inductive
+    def getcapacitiveComponent(self, other_probe):
+        t = self.channel.t - self.channel.currentStart
+        v1 = self.channel.v_filt
+        v2 = other_probe.channel.v_filt       
+        inductive = (v1 + v2)*0.5
+        return t, inductive
+    def getCurrent(self, other_probe=None, numPosts=8, backoff=10.):
+        if(other_probe is None):
+            t = self.channel.t - self.channel.currentStart
+            V = self.channel.v_filt
+        else:
+            t, V = self.getInductiveComponent(other_probe)
+        currentStartIndex = np.argmin( np.abs(t+backoff) )
+        t_trimmed = t[currentStartIndex:]
+        V_trimmed = V[currentStartIndex:]
+        intergrated = cumtrapz(V_trimmed, x=t_trimmed, initial=0.)
+        intergrated *= self.calibration
+        intergrated *= numPosts
+        return t_trimmed, intergrated
+    
+        
